b8aab15d3246d6f99b47145a51c65d754276d77f
Ian K. Wilkerson <ikwilker@go.olemiss.edu>
2017-04-02 11:38:51 -0500

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { Page1 } from '../pages/page1/page1';
+import { Page2 } from '../pages/page2/page2';
+import { AccountPage} from '../pages/account/account';
+import { SetsPage } from '../pages/sets/sets';
+import { AboutPage } from '../pages/about/about';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    Page1,
+    Page2,
+    AccountPage,
+    SetsPage,
+    AboutPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    Page1,
+    Page2,
+    AccountPage,
+    SetsPage,
+    AboutPage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
